75a7545a87747d0fb9264351971d4676572e9566
SUJIT KAR <sujitkar2005@gmail.com>
2026-08-22 16:58:07 +0530

    Delete Week-01_Python-Database-API/Day-4-File-Handling-PDF-JSON/01_file_handling/append_file.py

--- Week-01_Python-Database-API/Day-4-File-Handling-PDF-JSON/01_file_handling/append_file.py
+++
@@ -1,9 +0,0 @@
-# The following code demonstrates how to append text to an existing file in Python. It opens the file in append mode, writes a new line, and then closes the file.
-
-# f=open("demo1.txt", "a")
-# f.write("\nThis is an appended line.")
-# f.close()
-
-# Automatically close the file after appending to it
-with open("demo1.txt", "a") as f:
-    f.write("\nThis is an appended line.")

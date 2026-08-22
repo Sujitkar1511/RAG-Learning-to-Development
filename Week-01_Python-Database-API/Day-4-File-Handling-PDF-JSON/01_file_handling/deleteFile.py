4d1d59891c3346d0e2893fb0227abee504338a3f
SUJIT KAR <sujitkar2005@gmail.com>
2026-08-22 16:58:15 +0530

    Delete Week-01_Python-Database-API/Day-4-File-Handling-PDF-JSON/01_file_handling/deleteFile.py

--- Week-01_Python-Database-API/Day-4-File-Handling-PDF-JSON/01_file_handling/deleteFile.py
+++
@@ -1,7 +0,0 @@
-# the os module provides a way to interact with the operating system
-#  including file operations such as deleting files.
-# The os.remove() function is used to delete a file specified by its path.
-import os
-
-os.remove("demo2.txt")
-
